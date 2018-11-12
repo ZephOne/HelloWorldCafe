120b44e23e67f58074bd08530c1880314c5a83bd
ZephOne <erwan.colin@mines-albi.fr>
2018-11-12 23:11:11 +0100

    Initial Commit

---
+++ src/main/java/fr/imt/albi/gipsi/gsi/App.java
@@ -0,0 +1,13 @@
+package fr.imt.albi.gipsi.gsi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
